a32ddba9455bb8a3fc553144fe0fba12dea98ab6
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-03-15 10:27:59 +0530

    7

--- Ecommerce-follow/frontend/src/components/Card.jsx
+++ Ecommerce-follow/frontend/src/components/Card.jsx
@@ -3,7 +3,7 @@ import styles from './card.module.css';
 
 const Card = ({products}) =>  {
   return (
-    <div className={styles.card}>
+    <div className={styles.Card}>
         <img src={products.image} alt={products.title} />
         <h3>{products.title}</h3>
         <p>${products.price}</p>
